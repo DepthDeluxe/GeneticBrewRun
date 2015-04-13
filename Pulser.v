843e6cebf50924c4d6c4a75e97f3d371c870694a
DepthDeluxe <cheinzmann3@gmail.com>
2015-04-13 16:33:50 -0400

    added Pulser module haven't tested yet, pretty simple so not concerned

---
+++ Pulser.v
@@ -0,0 +1,37 @@
+module Pulser(
+  input clk,
+  input in,
+  output out
+);
+
+reg [1:0] state;
+reg [1:0] next_state;
+
+assign out = ( state == 1 )
+
+initial begin
+  state = 0;
+end
+
+always @ ( * )
+begin
+  if ( state == 0 )
+    if ( in )
+      state = 1;
+    else
+      state = 0;
+  else if ( state == 1 )
+    next_state = 2;
+  else
+    if ( in )
+      next_state = 3;
+    else
+      next_state = 0
+end
+
+always @ ( posedge clk )
+begin
+  state <= next_state;
+end
+
+endmodule
