6501eb320b5bd478753944708e453f4ef6a3d7e8
Colin Heinzmann <cheinzmann3@gmail.com>
2015-04-22 21:31:21 -0400

    fixed problems with  Pulser
    in always @ *, assignment should be to next_state
    forgot semicolons -_-

--- Pulser.v
+++ Pulser.v
@@ -7,7 +7,7 @@ module Pulser(
 reg [1:0] state;
 reg [1:0] next_state;
 
-assign out = ( state == 1 )
+assign out = ( state == 1 );
 
 initial begin
   state = 0;
@@ -17,16 +17,16 @@ always @ ( * )
 begin
   if ( state == 0 )
     if ( in )
-      state = 1;
+      next_state = 1;
     else
-      state = 0;
+      next_state = 0;
   else if ( state == 1 )
     next_state = 2;
   else
     if ( in )
       next_state = 3;
     else
-      next_state = 0
+      next_state = 0;
 end
 
 always @ ( posedge clk )
