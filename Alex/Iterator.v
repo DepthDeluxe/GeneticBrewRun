b6f4497f74fcc9bea951abef749997dedb116502
Colin Heinzmann <cheinzmann3@gmail.com>
2015-05-01 18:40:55 -0400

    overhauled Iterator module
    * implements changes from Swap module
    * added start and stop ins/outs

--- Alex/Iterator.v
+++ Alex/Iterator.v
@@ -18,23 +18,81 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module iterator(
+module Iterator(
     input clk,
+	 input start,
 	 input [31:0] prg_seed,
     input [149:0] parent,
-    output [749:0] family
+    output [749:0] family,
+	 output done
     );
-	 
-wire child_1;
-wire child_2;
-wire child_3;
-wire child_4;
 
-assign family = {parent, child_1, child_2, child_3, child_4};
+// the first member of the family is always going to be the parent
+assign family[149:0] = parent;
+	
+// state management
+reg [1:0] state, next_state;
+assign done = ( state == 2 );
+wire swap_start; assign swap_start = ( state == 1 );
+	
+// declare an array of Swap modules (sooo cleeeeean)
+reg [3:0] all_done;
+wire [3:0] swap_done;
+Swap swap_modules[3:0] (
+	.clk(clk),
+	.start(swap_start),
+	.prg_seed(prg_seed),
+	.parent(parent),
+	.mutant(family[749:150]),
+	.done(swap_done)
+);
 
-swap swap_00 (.clk(clk), .prg_seed(prg_seed), .parent(parent), .mutant(child_1));
-swap swap_01 (.clk(clk), .prg_seed(prg_seed), .parent(parent), .mutant(child_2));
-swap swap_02 (.clk(clk), .prg_seed(prg_seed), .parent(parent), .mutant(child_3));
-swap swap_03 (.clk(clk), .prg_seed(prg_seed), .parent(parent), .mutant(child_4));
+initial
+begin
+	state = 0; next_state = 0;
+	all_done = 0;
+end
+
+reg [2:0] i;
+always @ ( * )
+begin
+	// states
+	// 0: hold / wait to start
+	// 1: wait for all swap modules to stop
+	// 2: done
+	case ( state )
+	0:
+	begin
+		if ( start )
+			next_state = 1;
+		else
+			next_state = 0;
+			
+		// reset the all_done register
+		all_done = 0;
+	end
+	1:
+	begin
+		// only go on to end state when every single Swap module is done
+		if ( all_done == 4'b1111 )
+			next_state = 2;
+		else
+			next_state = 1;
+	
+		for ( i = 0; i < 4; i = i + 1 )
+			all_done[i] = all_done[i] | swap_done[i];
+	end
+	default:
+	begin
+		next_state = 0;
+		all_done = all_done;
+	end
+	endcase
+end
+
+always @ ( posedge clk )
+begin
+	state <= next_state;
+end
 
 endmodule
