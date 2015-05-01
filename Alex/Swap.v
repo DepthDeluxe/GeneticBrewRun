c7166c6dd98d5b88f5e5648bd428e2d93003d60d
Colin Heinzmann <cheinzmann3@gmail.com>
2015-05-01 16:11:43 -0400

    overhauled Swap module
    previous module didn't have start and stop options, messed around with
    the state machine and counters to properly swap a pseudorandom number of
    times

--- Alex/Swap.v
+++ Alex/Swap.v
@@ -1,4 +1,6 @@
 `timescale 1ns / 1ps
+`include "PseudorandomGenerator.v"
+
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -18,181 +20,131 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module swap(
+module Swap(
 	input clk,
+	input start,
 	input [31:0] prg_seed,
 	input [149:0] parent,
 	
 	output [4:0] debug_aug0,
 	output [4:0] debug_aug1,
-//	output [4:0] debug_selection,
 	output [149:0] debug_change,
 	output [2:0] debug_state,
 	
 	output reg [149:0] mutant,
-	output reg done
+	output done
 	);
+	
+assign done = ( state == 5 );
 
 reg [149:0] next_mutant;
-reg prg_start;
+reg [2:0] state, next_state;
+reg [2:0] counter, next_counter;
 
-wire [4:0] aug0;
-wire [4:0] aug1;
-
-reg [2:0] state;
-reg [2:0] next_state;
-wire [1:0] random_state; assign random_state = prg_seed[11:10]; 	//accounts for randomized number of swaps, 0-3
-
-reg next_done;
-
+// PRG controls
+wire [8:0] prg_val0, prg_val1;
+wire prg_start; assign prg_start = ( state == 1 || state == 2 );
+wire prg_done;
 PseudorandomGenerator prg_module0 (
 	.clk(clk),
 	.start(prg_start),
 	.in_seed(prg_seed),
-	.value(aug0),
-	.done(prg_done0)
+	.value(prg_val0),
+	.done(prg_done)
 	);
 PseudorandomGenerator prg_module1 (
 	.clk(clk),
 	.start(prg_start),
-	.in_seed(prg_seed+183),
-	.value(aug1),
-	.done(prg_done1)
+	.in_seed(~prg_seed),
+	.value(prg_val1)
 	);
-
-//debug assignment
-assign debug_aug0 = aug0;
-assign debug_aug1 = aug1;
-//assign debug_selection = parent[5*aug0 -: 5];		//if aug0 is 0, then takes bits 0:-5
-assign debug_change = parent ^ mutant;
-assign debug_state = state;
+	
+// indices
+wire [4:0] index0, index1;
+assign index0 = prg_val0[4:0];
+assign index1 = prg_val1[4:0];
 
 initial begin
-	mutant = 150'b0;
-	state = 0;
-	done = 0;
-	prg_start = 0;
+	mutant = 0; next_mutant = 0;
+	counter = 0; next_counter = 0;
+	state = 0; next_state = 0;
 end
 
-
+reg [4:0] i;
 always @ (*)
 begin
-	case (state)
-		0:
-			begin
-				next_mutant = mutant;
-				next_done = done;
-				
-				if ( done == 0 )
-					next_state = 1;
-				else
-					next_state = 0;
-			end
-					
-		1:		//wait for random number
-			begin
-				prg_start = 1;
-				next_mutant = mutant;		//retain mutant
-				next_done = done;				//retain done
-			
-				if ( prg_done0 == 1 && prg_done1 == 1 )
-				begin
-					next_state = 2;
-					prg_start = 0;
-				end else
-					next_state = 1;
-			end
-			
-		2:		//sample random vars until acceptable values are attained
-				//need 0-29
-			begin
-				next_mutant = mutant;		//retain mutant
-				next_done = done;				//retain done
-				prg_start = 0;
-				
-				//constrain random vars and random state, as long as done is 0
-				if (random_state != 0)
-					if (aug0 != aug1 && aug0 < 32 && aug0 > 0 && aug1 < 32 && aug1 > 0)
-						next_state = random_state + 2;
-					else
-						next_state = 1;
-				else
-					next_state = 2;
-			end
-			
-		3:		//first potential swap
-			begin
-				next_mutant = parent;		//set next_mutant
-				next_done = 0;		//not done
-				prg_start = 0;
-			
-				//set swapped indicies
-				next_mutant[5*aug0] = parent[5*aug1];
-				next_mutant[5*aug0 + 1] = parent[5*aug1 + 1];
-				next_mutant[5*aug0 + 2] = parent[5*aug1 + 2];
-				next_mutant[5*aug0 + 3] = parent[5*aug1 + 3];
-				next_mutant[5*aug0 + 4] = parent[5*aug1 + 4];
-				
-				next_mutant[5*aug1] = parent[5*aug0];
-				next_mutant[5*aug1 + 1] = parent[5*aug0 + 1];
-				next_mutant[5*aug1 + 2] = parent[5*aug0 + 2];
-				next_mutant[5*aug1 + 3] = parent[5*aug0 + 3];
-				next_mutant[5*aug1 + 4] = parent[5*aug0 + 4];
-				
-				next_state = 4;	//move to next state
-			end
-			
-		4:		//second potential swap
-			begin
-				next_mutant = parent;		//set next_mutant
-				next_done = 0;		//not done
-				prg_start = 0;
-				
-				//set swapped indicies
-				next_mutant[5*aug0] = parent[5*aug1];
-				next_mutant[5*aug0 + 1] = parent[5*aug1 + 1];
-				next_mutant[5*aug0 + 2] = parent[5*aug1 + 2];
-				next_mutant[5*aug0 + 3] = parent[5*aug1 + 3];
-				next_mutant[5*aug0 + 4] = parent[5*aug1 + 4];
-				
-				next_mutant[5*aug1] = parent[5*aug0];
-				next_mutant[5*aug1 + 1] = parent[5*aug0 + 1];
-				next_mutant[5*aug1 + 2] = parent[5*aug0 + 2];
-				next_mutant[5*aug1 + 3] = parent[5*aug0 + 3];
-				next_mutant[5*aug1 + 4] = parent[5*aug0 + 4];
-				
-				next_state = 5;	//move to next state
-			end
-			
-		5:		//third potential swap
-			begin
-				next_mutant = parent;		//set next_mutant
-				prg_start = 0;
-				
-				//set swapped indicies
-				next_mutant[5*aug0] = parent[5*aug1];
-				next_mutant[5*aug0 + 1] = parent[5*aug1 + 1];
-				next_mutant[5*aug0 + 2] = parent[5*aug1 + 2];
-				next_mutant[5*aug0 + 3] = parent[5*aug1 + 3];
-				next_mutant[5*aug0 + 4] = parent[5*aug1 + 4];
-				
-				next_mutant[5*aug1] = parent[5*aug0];
-				next_mutant[5*aug1 + 1] = parent[5*aug0 + 1];
-				next_mutant[5*aug1 + 2] = parent[5*aug0 + 2];
-				next_mutant[5*aug1 + 3] = parent[5*aug0 + 3];
-				next_mutant[5*aug1 + 4] = parent[5*aug0 + 4];
+	// default behavior: keep mutant the same
+	next_mutant = mutant;
+	next_counter = counter;
 
-				next_done = 1;		//done
-				next_state = 0;
+	// states
+	// 0: hold state
+	// 1: generate random counter value, wait for PRG
+	// 2: generate indices, wait for PRG
+	// 3: PRG acceptable check
+	// 4: swaps indices
+	// 5: done state
+	case ( state )
+		0:	// hold state
+			if ( start )
+			begin
+				next_state = 1;
+				next_mutant = parent;
 			end
+			else
+				next_state = 0;
+				
+		1: // wait for PRG, generate number of swaps we will do
+			if ( prg_done )
+			begin
+				next_state = 3;
+				
+				// will swap 1-3 times
+				next_counter = prg_val0[7:5] + 1;
+			end
+			else
+				next_state = 1;
+
+		2:	// wait for PRG, generate indices
+			if ( prg_done )
+				next_state = 3;
+			else
+				next_state = 2;
+			
+		3:	// PRG acceptable check	
+			//sample random vars until acceptable values are attained
+			//need 0-29
+			if (index0 != index1 && index0 < 30 && index1 < 30)
+				next_state = 4;
+			else
+				next_state = 2;
+			
+		4: // perform swap
+			begin			
+				//set swapped indicies
+				for ( i = 0; i < 5; i = i + 1 )
+				begin
+					next_mutant[5 * index0 + i] = mutant[5 * index1 + i];
+					next_mutant[5 * index1 + i] = mutant[5 * index0 + i];
+				end
+				
+				// decrement counter, if counter is zero, move to done state
+				next_counter = counter - 1;
+				if ( counter == 0 )
+					next_state = 5;
+				else
+					next_state = 2;
+			end
+		5:
+			next_state = 0;
 	endcase
 end
 
 always @ (posedge clk)
 begin
 	mutant <= next_mutant;
+	counter <= next_counter;
 	state <= next_state;
-	done <= next_done;
 end
 
 endmodule
--- Alex/Swap_Test.v
+++ Alex/Swap_Test.v
@@ -22,31 +22,23 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module swapTest;
+module Swap_Test;
 
 	// Inputs
 	reg clk;
+	reg start;
 	reg [31:0] prg_seed;
 	reg [149:0] parent;
-	
-	// Outputs
-//	wire [4:0] debug_aug0;
-//	wire [4:0] debug_aug1;
-//	wire [4:0] debug_selection;
-//	wire [149:0] debug_change;
 
 	wire [149:0] mutant;
 	wire done;
 
 	// Instantiate the Unit Under Test (UUT)
-	swap uut (
-		.clk(clk), 
+	Swap uut (
+		.clk(clk),
+		.start(start),
 		.prg_seed(prg_seed), 
 		.parent(parent),
-//		.debug_aug0(debug_aug0),
-//		.debug_aug1(debug_aug1),
-//		.debug_selection(debug_selection),
-//		.debug_change(debug_change),
 		.mutant(mutant),
 		.done (done)
 	);
@@ -57,14 +49,20 @@ module swapTest;
 		// Initialize Inputs
 		clk = 0;
 		prg_seed = 0;
+		start = 0;
 		parent = 0;
 
 		// Wait 100 ns for global reset to finish
-		#100;
+		#1000;
         
-		// Add stimulus here
-		prg_seed = 32'd3124684136;
+		prg_seed = 3124684136;
 		parent = 150'b000000000100010000110010000101001100011101000010010101001011011000110101110011111000010001100101001110100101011011010111110001100111010110111110011101;
+		#20;
+		start = 1;
+		#20
+		start = 0;
+
+		#100000;
 
 	end
       
